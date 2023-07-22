7ce8682a9395cede3c0018feec9f6e2b454f7620
lionel.vest@adaris.org <lionel.vest@adaris.org>
2023-07-22 22:15:05 +0200

    V 5.00

--- status.sh
+++ status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /etc/optimus/functions.sh
 
-output $OUTPUT_MODE '' 'green' 200 'optimus-installer' 100
-if [ ! -z $LAST_UPGRADE ]; then output $OUTPUT_MODE '' 'green' 200 'upgrade' 100; fi
+output $OUTPUT_MODE "optimus-installer" "green" 200 "optimus-installer" 100
+if [ ! -z $LAST_UPGRADE ]; then output $OUTPUT_MODE "upgrade" "green" 200 "upgrade" 100; fi
