a648c8e404dda97077a9de75dd6059adcfb53457
lionel.vest@adaris.org <lionel.vest@adaris.org>
2023-07-22 16:44:03 +0200

    V 5.00

--- upgrade/d10_to_d11.sh
+++ upgrade/d10_to_d11.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo_green "==== MISE A JOUR DE DEBIAN 10 VERS DEBIAN 11 ===="
+
+. /etc/os-release
+if [ $ID == 'debian' ] && [ $VERSION_ID == 10 ]
+then
+  sed -i 's/buster/bullseye/g' /etc/apt/sources.list
+  sed -i 's/bullseye\/updates/bullseye-security/g' /etc/apt/sources.list
+fi
+
 echo_magenta "Update"
 DEBIAN_FRONTEND=noninteractive verbose apt-get -qq -y --allow-releaseinfo-change update
 
@@ -9,4 +18,3 @@ echo_magenta "Dist-Upgrade"
 DEBIAN_FRONTEND=noninteractive verbose apt-get -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 
 update_conf LAST_UPGRADE $(date +'%Y%m%d')
-fi
