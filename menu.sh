6a7d0f02b53ce42692be5f780142548fba731c88
lionel.vest@adaris.org <lionel.vest@adaris.org>
2023-09-05 15:10:49 +0200

    V 1.00

--- menu.sh
+++ menu.sh
@@ -52,8 +52,8 @@ tput cup 39 3; echo -ne "\033[46;30m Select Option : \e[0m"; tput cup 25 21
 
 if [ -d /etc/docker ] && [ $( docker ps -a | grep optimus-devtools | wc -l ) -gt 0 ]
 then
-  tput cup 41 3; echo "1. Reconstructeur d'image DOCKER (DEV TOOLS)"
-  tput cup 42 3; echo "2. Connexion à git.cybertron.fr (DEV TOOLS)"
+  tput cup 41 3; echo "1. Connexion à git.cybertron.fr (DEV TOOLS)"
+  tput cup 42 3; echo "2. Reconstructeur d'image DOCKER (DEV TOOLS)"
 fi
 
 tput cup 44 3; echo_magenta "Il est rappelé que le logiciel OPTIMUS et ses composants sont des logiciels libres."
@@ -302,13 +302,13 @@ case "$y" in
   1)
 	tput reset
 	clear
-	source /etc/optimus/optimus-devtools/build.sh
+	source /etc/optimus/optimus-devtools/git_login.sh
 	;;
-
+	
   2)
 	tput reset
 	clear
-	source /etc/optimus/optimus-devtools/git_login.sh
+	source /etc/optimus/optimus-devtools/build.sh
 	;;
 
   '')
--- optimus-devtools/git_login.sh
+++ optimus-devtools/git_login.sh
@@ -2,21 +2,27 @@
 source /etc/optimus/functions.sh
 
 echo
-echo_magenta "Connexion au registre de conteneurs https://git.cybertron.fr:5050"
+echo_cyan "Connexion au registre de conteneurs https://git.cybertron.fr:5050"
 docker login https://git.cybertron.fr:5050
 
 echo
-echo_magenta "Génération d'une clé développeur ED25519 pour l'utilisateur debian"
-if [ ! -f /home/debian/.ssh/optimusdev ]
+echo
+echo_cyan "Connexion au dépot git.cybertron.fr"
+if [ ! -f /home/debian/.ssh/id_ed25519 ]
 then
-    ssh-keygen -t ed25519 -f "/home/debian/.ssh/optimusdev" -C "$DOMAIN"
-    chmod debian:debian /home/debian/.ssh/optimusdev
-    chmod debian:debian /home/debian/.ssh/optimusdev.pub
+    echo_magenta "Génération d'une clé développeur ED25519 pour l'utilisateur debian"
+    su -c 'ssh-keygen -t ed25519 -C "debian@$DOMAIN"' debian
 fi
+
 echo
-echo_green "Vous trouverez ci-dessous la clé publique de ce serveur à copier sur https://git.cybertron.fr :"
+echo_magenta "Vous trouverez ci-dessous la clé publique de ce serveur à copier sur https://git.cybertron.fr/-/profile/keys :"
 echo
-echo_yellow "$(cat /home/debian/.ssh/optimusdev.pub)"
+echo_yellow "$(cat /home/debian/.ssh/id_ed25519.pub)"
+echo
+
+read -p "Après avoir copié la clé, appuyez sur [ENTREE] pour tester la connexion..."
+echo
+su -c "ssh -T git@git.cybertron.fr" debian
 echo
 
 read -p "Appuyez sur [ENTREE] pour continuer..."
