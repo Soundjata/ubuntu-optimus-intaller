4f82e34b9fbc414e5276e9ad4b2eb9710ad5a8db
lionel.vest@adaris.org <lionel.vest@adaris.org>
2023-09-05 14:28:40 +0200

    V 1.00

---
+++ optimus-devtools/git_login.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source /etc/optimus/functions.sh
+
+echo
+echo_magenta "Connexion au registre de conteneurs https://git.cybertron.fr:5050"
+docker login https://git.cybertron.fr:5050
+
+echo
+echo_magenta "Génération d'une clé développeur ED25519 pour l'utilisateur debian"
+if [ ! -f /home/debian/.ssh/optimusdev ]
+then
+    ssh-keygen -t ed25519 -f "/home/debian/.ssh/optimusdev" -C "$DOMAIN"
+fi
+echo
+echo_green "Vous trouverez ci-dessous la clé publique de ce serveur à copier sur https://git.cybertron.fr :"
+echo
+echo_yellow "$(cat /home/debian/.ssh/optimusdev.pub)"
+echo
+
+read -p "Appuyez sur [ENTREE] pour continuer..."
