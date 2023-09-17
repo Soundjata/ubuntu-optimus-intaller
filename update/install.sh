281fe4e3d700d2a73130b88e3b92e5127bc80e2f
Lionel VEST <lionel.vest@adaris.org>
2023-09-17 15:40:26 +0000

    V 5.00

--- update/install.sh
+++ update/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-cd /
-rm -R /etc/optimus
-mkdir /etc/optimus
+
 
 if [ -d /etc/docker ] && [ $( docker ps -a | grep optimus-devtools | wc -l ) -gt 0 ]
 then
     echo_red "optimus-installer ne peut pas être mis à jour lorsque le serveur est en mode développement. Utilisez GIT pour cette tâche"
 else
+    cd /
+    rm -R /etc/optimus
+    mkdir /etc/optimus
     git clone https://git.cybertron.fr/optimus/optimus-installer /etc/optimus
+    chmod +x /etc/optimus/menu.sh
+    source /etc/optimus/menu.sh
 fi
-
-chmod +x /etc/optimus/menu.sh
-source /etc/optimus/menu.sh
