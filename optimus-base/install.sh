6857bd2b148c641ae00d7ba5ef792ed1667d5975
Lionel VEST <lionel.vest@adaris.org>
2023-09-17 05:57:47 +0000

    V 5.00

--- optimus-base/install.sh
+++ optimus-base/install.sh
@@ -4,7 +4,7 @@ source /etc/optimus/functions.sh
 NAME="optimus-base"
 source /etc/optimus/optimus-init/container_installer.sh
 
-if [ $OUTPUT_MODE != "JSON" ]
+if [ $OUTPUT_MODE != "json" ]
 then 
 	echo
 	source /etc/optimus/create_admin/install.sh
