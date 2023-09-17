7967074aed6dae176d33766f165e86f2facda163
Lionel VEST <lionel.vest@adaris.org>
2023-09-17 15:41:17 +0000

    V 5.00

--- update/install.sh
+++ update/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-if [ -d /etc/docker ] && [ $( docker ps -a | grep optimus-devtools | wc -l ) -gt 0 ]
+if [ -d /etc/optimus/.git ]
 then
     echo_red "optimus-installer ne peut pas être mis à jour lorsque le serveur est en mode développement. Utilisez GIT pour cette tâche"
 else
