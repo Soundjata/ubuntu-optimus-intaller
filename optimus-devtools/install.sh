5367a10eeb8c7100a7fcb067e9c2a4a7d6811ad7
Lionel VEST <lionel.vest@adaris.org>
2023-09-17 15:23:40 +0000

    V 5.00

--- optimus-devtools/install.sh
+++ optimus-devtools/install.sh
@@ -13,6 +13,16 @@ then
 fi
 verbose mariadb -u root -p$MARIADB_ROOT_PASSWORD -e "GRANT ALL ON *.* to 'root'@'%' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD' WITH GRANT OPTION;"
 
+output $OUTPUT_MODE "Création d'un swap de 512Mo" "magenta" 200 "optimus-devtools" 30
+if [[ $(sudo /usr/sbin/swapon -s) != *"/var/swap.img"* ]]
+then
+	sudo dd if=/dev/zero of=/var/swap.img bs=1024 count=512k
+	sudo chmod 600 /var/swap.img
+	sudo mkswap /var/swap.img
+	sudo swapon /var/swap.img
+	sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+fi
+
 output $OUTPUT_MODE "Ajout de l'utilisateur debian au groupe www-data" "magenta" 200 "optimus-devtools" 45
 verbose usermod -a -G www-data debian
 
@@ -21,49 +31,46 @@ output $OUTPUT_MODE "Attribution des droits en écriture sur les dépôts" "mage
 verbose chmod 775 -R /srv/optimus
 verbose chown www-data:www-data -R /srv/optimus
 
-if [ "$OUTPUT_MODE" != "json" ]
+echo
+echo
+echo_cyan "Connexion au registre de conteneurs https://git.cybertron.fr:5050"
+
+echo
+echo_green "Veuillez indiquer votre identifiant git.cybertron.fr :"
+read GIT_USERNAME
+
+echo
+echo_green "Veuillez indiquer votre mot de passe git.cybertron.fr :"
+read GIT_PASSWORD
+
+docker logout
+echo
+su -c "docker login https://git.cybertron.fr:5050 -u '$GIT_USERNAME' -p '$GIT_PASSWORD'" debian
+
+echo
+echo
+echo_cyan "Connexion au dépot git.cybertron.fr"
+su -c 'git config --global user.name "'$GIT_USERNAME'"' debian
+su -c 'git config --global user.email "'$GIT_USERNAME'"' debian
+su -c 'git config --global --add safe.directory "*"' debian
+if [ ! -f /home/debian/.ssh/id_ed25519 ]
 then
-	echo
-	echo
-	echo_cyan "Connexion au registre de conteneurs https://git.cybertron.fr:5050"
-
-	echo
-	echo_green "Veuillez indiquer votre identifiant git.cybertron.fr :"
-    read GIT_USERNAME
-
-	echo
-	echo_green "Veuillez indiquer votre mot de passe git.cybertron.fr :"
-    read GIT_PASSWORD
-
-	docker logout
-	echo
-	su -c "docker login https://git.cybertron.fr:5050 -u '$GIT_USERNAME' -p '$GIT_PASSWORD'" debian
-
-	echo
-	echo
-	echo_cyan "Connexion au dépot git.cybertron.fr"
-	su -c 'git config --global user.name "'$GIT_USERNAME'"' debian
-	su -c 'git config --global user.email "'$GIT_USERNAME'"' debian
-	su -c 'git config --global --add safe.directory "*"' debian
-	if [ ! -f /home/debian/.ssh/id_ed25519 ]
-	then
-		echo_magenta "Génération d'une clé développeur ED25519 pour l'utilisateur debian"
-		su -c 'ssh-keygen -t ed25519 -f "/home/debian/.ssh/id_ed25519" -N "" -C "debian@$DOMAIN"' debian
-	fi
-
-	echo
-	echo_magenta "Vous trouverez ci-dessous la clé publique de ce serveur à copier sur https://git.cybertron.fr/-/profile/keys :"
-	echo
-	echo_yellow "$(cat /home/debian/.ssh/id_ed25519.pub)"
-	echo
-
-	read -p "Après avoir copié la clé, appuyez sur [ENTREE] pour tester la connexion..."
-	echo
-	su -c "ssh -T -o 'StrictHostKeyChecking no' git@git.cybertron.fr" debian
-	echo
-
-	read -p "Appuyez sur [ENTREE] pour continuer..."
+	echo_magenta "Génération d'une clé développeur ED25519 pour l'utilisateur debian"
+	su -c 'ssh-keygen -t ed25519 -f "/home/debian/.ssh/id_ed25519" -N "" -C "debian@$DOMAIN"' debian
 fi
+
+echo
+echo_magenta "Vous trouverez ci-dessous la clé publique de ce serveur à copier sur https://git.cybertron.fr/-/profile/keys :"
+echo
+echo_yellow "$(cat /home/debian/.ssh/id_ed25519.pub)"
+echo
+
+read -p "Après avoir copié la clé, appuyez sur [ENTREE] pour tester la connexion..."
+echo
+su -c "ssh -T -o 'StrictHostKeyChecking no' git@git.cybertron.fr" debian
+echo
+
+read -p "Appuyez sur [ENTREE] pour continuer..."
 
 output $OUTPUT_MODE
 output $OUTPUT_MODE "Réinstallation du repo optimus-installer (DEV MODE)" "magenta" 200 "optimus-devtools" 90
