fdbf88e4a412c102da8a4e1a8288b54c0908952e
Lionel VEST <lionel.vest@adaris.org>
2023-09-17 15:29:46 +0000

    V 5.00

--- update/install.sh
+++ update/install.sh
@@ -5,7 +5,7 @@ mkdir /etc/optimus
 
 if [ -d /etc/docker ] && [ $( docker ps -a | grep optimus-devtools | wc -l ) -gt 0 ]
 then
-    git clone git@git.cybertron.fr/optimus/optimus-installer /etc/optimus
+    git clone git@git.cybertron.fr:optimus/optimus-installer.git /etc/optimus
 else
     git clone https://git.cybertron.fr/optimus/optimus-installer /etc/optimus
 fi
